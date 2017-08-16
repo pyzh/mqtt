144601af547d39810b2ac93a8973b65e46e71c67
ne-luboff <ne-luboff@yandex.ua>
2017-08-16 19:28:49 +0300

    Update subscription logic.

--- src/n2o_vnode.erl
+++ src/n2o_vnode.erl
@@ -58,7 +58,7 @@ proc({publish, To, Request},
     Bert   = binary_to_term(Request,[safe]),
     Return = case Addr of
          [ Origin, Vsn, Node, Module, Username, Id, Token | _ ] ->
-         From = nitro:to_binary(["actions/1/",Module,"/",Id]),
+         From = nitro:to_binary(["actions/", Vsn, Module,"/",Id]),
          Sid  = nitro:to_binary(Token),
          % io:format("Module: ~p~n",[Module]),
          Ctx  = #cx { module=fix(Module), session=Sid, node=Node, params=Id, client_pid=C, from = From, vsn = Vsn},
@@ -75,8 +75,7 @@ proc({publish, To, Request},
 % On connection subscribe to Server Events: "events/:node/#"
 
 proc({mqttc, C, connected}, State=#handler{name=Name,state=C,seq=S}) ->
-    emqttc:subscribe(C, nitro:to_binary([<<"events/1/">>,
-                        nitro:to_list(Name),"/#"]), 2),
+    emqttc:subscribe(C, nitro:to_binary([<<"events/+/">>, nitro:to_list(Name),"/#"]), 2),
     {ok, State#handler{seq = S+1}};
 
 % Examples:
