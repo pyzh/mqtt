db9b74dbef855c629be6759c3565ec4763125aee
qomputer <qomputer@gmail.com>
2018-06-26 12:01:36 +0300

    Remove hooks for session.terminated and client.disconnected

--- src/n2o.erl
+++ src/n2o.erl
@@ -16,13 +16,13 @@
 
 load(Env) ->
     emqttd:hook('client.connected',    fun ?MODULE:on_client_connected/3,     [Env]),
-    emqttd:hook('client.disconnected', fun ?MODULE:on_client_disconnected/3,  [Env]),
+ %   emqttd:hook('client.disconnected', fun ?MODULE:on_client_disconnected/3,  [Env]),
     emqttd:hook('client.subscribe',    fun ?MODULE:on_client_subscribe/4,     [Env]),
     emqttd:hook('client.unsubscribe',  fun ?MODULE:on_client_unsubscribe/4,   [Env]),
     emqttd:hook('session.created',     fun ?MODULE:on_session_created/3,      [Env]),
     emqttd:hook('session.subscribed',  fun ?MODULE:on_session_subscribed/4,   [Env]),
     emqttd:hook('session.unsubscribed',fun ?MODULE:on_session_unsubscribed/4, [Env]),
-    emqttd:hook('session.terminated',  fun ?MODULE:on_session_terminated/4,   [Env]),
+%    emqttd:hook('session.terminated',  fun ?MODULE:on_session_terminated/4,   [Env]),
     emqttd:hook('message.publish',     fun ?MODULE:on_message_publish/2,      [Env]),
     emqttd:hook('message.delivered',   fun ?MODULE:on_message_delivered/4,    [Env]),
     emqttd:hook('message.acked',       fun ?MODULE:on_message_acked/4,        [Env]).
@@ -304,7 +304,7 @@ unsubscribe_cli(ClientId, TopicTable)->
         true  -> DelFun();
         false -> mnesia:transaction(DelFun)
     end,
-    [(not ets:member(mqtt_subscriber, Topic)) andalso emqttd_router:del_route(Topic) || {Topic,Qos} <- TopicTable ].
+    [(not ets:member(mqtt_subscriber, Topic)) andalso [emqttd_router:del_route(Route)|| Route<-mnesia:dirty_read({mqtt_route, Topic})] || {Topic,Qos} <- TopicTable ].
 
 
 get_vnode(ClientId) -> get_vnode(ClientId, []).
